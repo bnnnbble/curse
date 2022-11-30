8cc694b9fdf3a1ffae5a57eaa07f2778f5de2396
neuromancer <neuromancer402@gmail.com>
2022-11-30 22:15:50 +0500

    провер очка

--- kiruha_the_best.js
+++ kiruha_the_best.js
@@ -1,48 +1,61 @@
+let check = false;
+
 canvas = document.getElementById("hui");
 canvas.style.display = "none";
+
 function isEmail() {
-  canvas.style.display = "";
+  try{
+    canvas.style.display = "";
+    check = true;
+    var xyValues = [
+      { x: 10, y: 7 },
+      { x: 60, y: 8 },
+      { x: 70, y: 8 },
+      { x: 20, y: 9 },
+      { x: 90, y: 9 },
+      { x: 100, y: 9 },
+    ];
 
-  var xyValues = [
-    { x: 10, y: 7 },
-    { x: 60, y: 8 },
-    { x: 70, y: 8 },
-    { x: 20, y: 9 },
-    { x: 90, y: 9 },
-    { x: 100, y: 9 },
-  ];
+    let x1 = document.getElementById("x1").value,
+      x2 = document.getElementById("x2").value,
+      y1 = document.getElementById("y1").value,
+      y2 = document.getElementById("y2").value;
 
-  let x1 = document.getElementById("x1").value,
-    x2 = document.getElementById("x2").value,
-    y1 = document.getElementById("y1").value,
-    y2 = document.getElementById("y2").value;
-
-  new Chart("hui", {
-    type: "scatter",
-    data: {
-      datasets: [
-        {
-          pointRadius: 3,
-          pointBackgroundColor: "white",
-          data: xyValues,
-        },
-      ],
-    },
-    options: {
-      legend: { display: false },
-      scales: {
-        xAxes: [{ ticks: { min: Number(x1), max: Number(x2) } }],
-        yAxes: [{ ticks: { min: Number(y1), max: Number(y2) } }],
+    new Chart("hui", {
+      type: "scatter",
+      data: {
+        datasets: [
+          {
+            pointRadius: 3,
+            pointBackgroundColor: "white",
+            data: xyValues,
+          },
+        ],
       },
-    },
-  });
+      options: {
+        legend: { display: false },
+        scales: {
+          xAxes: [{ ticks: { min: Number(x1), max: Number(x2) } }],
+          yAxes: [{ ticks: { min: Number(y1), max: Number(y2) } }],
+        },
+      },
+    });
+  }catch{
+    check = false;
+    alert("Не удалось построить график")
+  }
 }
 
 function scatterChart() {}
 
 function saveImg() {
-  let a = document.createElement("a");
-  a.download = "screenshot.png";
-  a.href = canvas.toDataURL("image/png");
-  a.click();
+  if(check === true){
+    let a = document.createElement("a");
+    a.download = "screenshot.png";
+    a.href = canvas.toDataURL("image/png");
+    a.click();
+  }
+  else{
+    alert("Нечего сохранять :(");
+  }
 }
