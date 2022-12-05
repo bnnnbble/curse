e82d124a606601b9b86136dec53e58d5f2723be9
Neuromancer404 <73753089+Neuromancer404@users.noreply.github.com>
2022-12-05 11:16:15 +0500

    Update xlsxReader.js

--- script/xlsxReader.js
+++ script/xlsxReader.js
@@ -13,7 +13,7 @@ function excelRunner(file){
       workbook.SheetNames.forEach(function(sheetName) {
         // Here is your object
         var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
-        var json_object = JSON.stringify(XL_row_object);
+        var json_object = JSON.stringify(XL_row_object[0]);
         console.log(json_object);
 
       })
